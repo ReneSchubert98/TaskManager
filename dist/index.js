c6f21ac076c47e5c8c5ec61f02f4da59ed53349d
René Schubert <Rene.Schubert@m102214741d.fritz.box>
2024-10-04 19:53:28 +0200

    Anpassung

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _idb = require("idb");
-var dbPromise = (0, _idb.openDB)('task-manager-db', 1, {
-  upgrade: function upgrade(db) {
-    db.createObjectStore('tasks');
-  }
-});
--- src/index1.0.js
+++
@@ -1,7 +0,0 @@
-import { openDB } from 'idb';
-
-const dbPromise = openDB('task-manager-db', 1, {
-  upgrade(db) {
-    db.createObjectStore('tasks');
-  },
-});
